e7a6b57cc66271fa0e09ac58daad65449b3a1496
Amit Keinan <amitk2612@gmail.com>
2021-12-31 18:32:01 +0200

    good balance between the networks

--- ex3/gan/gan_models.py
+++ ex3/gan/gan_models.py
@@ -8,13 +8,15 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         self.linear1 = nn.Linear(int(input_dim), 64)
-        self.linear2 = nn.Linear(64, int(output_dim))
+        self.linear2 = nn.Linear(64, 32)
+        self.linear3 = nn.Linear(32, int(output_dim))
 
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.relu(self.linear1(x))
-        x = self.linear2(x)
+        x = self.relu(self.linear2(x))
+        x = self.linear3(x)
         return x
 
 
